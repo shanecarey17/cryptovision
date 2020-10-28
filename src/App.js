91b4a9730c38a676964372c97d7803ea63e90fe0
Shane Carey <shane.carey@Shanes-iMac.local>
2020-10-28 16:09:56 -0500

    volatility

--- src/App.js
+++ src/App.js
@@ -4,16 +4,57 @@ import React from 'react';
 
 import {Line} from 'react-chartjs-2';
 
+function average(data){
+  var sum = data.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+
+  var avg = sum / data.length;
+  return avg;
+}
+
+function standardDeviation(values){
+  var avg = average(values);
+  
+  var squareDiffs = values.map(function(value){
+    var diff = value - avg;
+    var sqrDiff = diff * diff;
+    return sqrDiff;
+  });
+  
+  var avgSquareDiff = average(squareDiffs);
+
+  var stdDev = Math.sqrt(avgSquareDiff);
+  return stdDev / avg;
+}
+
+function getVolatility(series) {
+    let seriesSorted = series.sort((a, b) => a.x - b.x);
+
+    return seriesSorted.map((e, i) => {
+      let dataSet = seriesSorted.slice(i - 30, i + 1).map(e => e.y);
+      let stdDev = standardDeviation(dataSet);
+
+      return {
+          x: e.x,
+          y: stdDev
+      }
+    });
+  }
+
 class App extends React.Component {
   constructor() {    
     super();
 
     this.state = {
-      chartData: {},
-      chartOptions: {
+      priceData: {},
+      volatilityData: {}
+    }
+
+    this.chartOptions = {
         title: {
           display: true,
-          text: 'BTC',
+          text: 'Price',
           fontSize: 20
         },
         legend: {
@@ -28,8 +69,27 @@ class App extends React.Component {
                 }
             }]
         }
-      }
-    }
+      };
+
+      this.volatilityOptions = {
+        title: {
+          display: true,
+          text: 'Volatility',
+          fontSize: 20
+        },
+        legend: {
+          display: true,
+          position: 'right'
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'month'
+                }
+            }]
+        }
+      };
 
     this.startDate = new Date();
     this.startDate.setFullYear(this.startDate.getFullYear() - 1);
@@ -67,18 +127,30 @@ class App extends React.Component {
       .then(res => res.ok ? res : Promise.reject(res))
       .then(res => res.json())
       .then((raw) => {
+          console.log(raw);
+
           let series = raw.data['Time Series (Daily)'];
 
           let keys = Object.keys(series).filter((key) => {
               let date = new Date(key);
               return date <= this.endDate && date >= this.startDate;
-          }).sort((a, b) => +Date(b) - +Date(a));
+          }).sort((a, b) => new Date(a) - new Date(b));
 
+          let splitCoeff = 1;
           let data = keys.reduce((d, key) => {
+              let close = Number(series[key]['4. close']);
+              let sf = Number(series[key]['8. split coefficient']);
+
+              splitCoeff = splitCoeff * sf;
+              if (sf > 1) {
+                console.log(splitCoeff);
+              }
+
               d.push({
                   x: new Date(key),
-                  y: Number(series[key]['4. close'])
+                  y: close * splitCoeff
               });
+
               return d;
           }, []);
 
@@ -88,13 +160,12 @@ class App extends React.Component {
               backgroundColor: color,
               borderColor: color
           }
-      })
+      });
   }
 
   componentDidMount() {
     let dataTasks = [
         this.requestBTCData(), 
-        //this.requestStockData('AAPL', 'blue'), 
         this.requestStockData('AMZN', 'yellow'),
         this.requestStockData('GOOGL', 'green'),
         this.requestStockData('GLD', 'brown'),
@@ -107,7 +178,8 @@ class App extends React.Component {
       .then((dataArr) => {
           console.log(dataArr);
         this.setState({
-          chartData: {
+          // Prices
+          priceData: {
             datasets: dataArr.map((data) => {
               return {
                   label: data.label,
@@ -119,6 +191,20 @@ class App extends React.Component {
                   data: data.series
               };
             })
+          },
+
+          // Volatility
+          volatilityData: {
+              datasets: dataArr.map((data) => {
+                  return {
+                      label: data.label,
+                      fill:false,
+                      backgroundColor: data.backgroundColor,
+                      borderColor: data.borderColor,
+                      borderWidth: 2,
+                      data: getVolatility(data.series)
+                  }
+              })
           }
         });
       });
@@ -127,7 +213,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Line data={this.state.chartData} options={this.state.chartOptions}/>
+        <Line data={this.state.priceData} options={this.chartOptions}/>
+        <Line data={this.state.volatilityData} options={this.volatilityOptions}/>
       </div>
     );
   }
